83ad2fd18f46f1393f8d2ad898779b24e634a081
jihchi <achi@987.tw>
2020-11-15 15:20:32 +0800

    Directly inline constants

--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,20 @@ const KQ: f32 = 0.1957;
 
 #[derive(Debug, PartialEq)]
 pub struct YIQ {
-    pub y: f32, // luminance, in range [0, 1]
-    pub i: f32, // hue of color, in range ~ [-0.5, 0.5]
-    pub q: f32, // saturation of color, in range ~ [-0.5, 0.5]
+    y: f32, // luminance, in range [0, 1]
+    i: f32, // hue of color, in range ~ [-0.5, 0.5]
+    q: f32, // saturation of color, in range ~ [-0.5, 0.5]
 }
 
 impl YIQ {
     pub fn from_rgb(rgb: &[u8; 3]) -> Self {
-        let matrix: [[f32; 3]; 3] = [
-            [0.29889531, 0.58662247, 0.11448223],
-            [0.59597799, -0.27417160, -0.32180189],
-            [0.21147019, -0.52261711, 0.31114694],
-        ];
-
         let r = rgb[0] as f32;
         let g = rgb[1] as f32;
         let b = rgb[2] as f32;
 
-        let y = matrix[0][0] * r + matrix[0][1] * g + matrix[0][2] * b;
-        let i = matrix[1][0] * r + matrix[1][1] * g + matrix[1][2] * b;
-        let q = matrix[2][0] * r + matrix[2][1] * g + matrix[2][2] * b;
+        let y = 0.29889531 * r + 0.58662247 * g + 0.11448223 * b;
+        let i = 0.59597799 * r + -0.27417160 * g + -0.32180189 * b;
+        let q = 0.21147019 * r + -0.52261711 * g + 0.31114694 * b;
 
         Self { y, i, q }
     }
