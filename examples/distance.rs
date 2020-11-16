a261294ed4e7daf312784a2911c55788ef17bdf5
jihchi <achi@987.tw>
2020-11-16 19:50:12 +0800

    Fix a commit typo

--- examples/distance.rs
+++ examples/distance.rs
@@ -6,8 +6,11 @@ fn main() -> Result<()> {
     let left = YIQ::from_rgb(&image::Rgb([0, 0, 0]));
     let right = YIQ::from_rgb(&image::Rgb([255, 255, 255]));
 
-    println!("Squared: {}", left.squared_distance(&right));
-    println!("Square Root: {}", left.square_root_distance(&right));
+    println!(
+        "Squared: {}, Square Root: {}",
+        left.squared_distance(&right),
+        left.square_root_distance(&right)
+    );
 
     Ok(())
 }
