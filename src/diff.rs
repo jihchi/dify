d59e1f51da7807d6ac488ae140cc55219af3b2fd
jihchi <achi@987.tw>
2020-11-23 21:36:55 +0800

    Don't allocate output buffer before we know whether any difference exists

--- src/diff.rs
+++ src/diff.rs
@@ -76,21 +76,14 @@ pub fn run(params: &RunParams) -> Result<Option<u32>> {
 
     let threshold = MAX_YIQ_POSSIBLE_DELTA * params.threshold * params.threshold;
     let (width, height) = left_dimensions;
+    let pixels: Vec<(u32, u32, &Rgba<u8>)> = left_image.enumerate_pixels().collect();
 
-    let mut output_image = match params.output_image_base {
-        Some(cli::OutputImageBase::LeftImage) => left_image.clone(),
-        Some(cli::OutputImageBase::RightImage) => right_image.clone(),
-        None => RgbaImage::new(width, height),
-    };
-
-    let mut diffs: u32 = 0;
-
-    for (x, y, left_pixel) in left_image.enumerate_pixels() {
+    let results = pixels.iter().map(|(x, y, left_pixel)| {
         let result = {
-            if right_image.in_bounds(x, y) {
-                let right_pixel = right_image.get_pixel(x, y);
+            if right_image.in_bounds(*x, *y) {
+                let right_pixel = right_image.get_pixel(*x, *y);
 
-                if left_pixel == right_pixel {
+                if *left_pixel == right_pixel {
                     DiffResult::Identical
                 } else {
                     let left_pixel = YIQ::from_rgba(left_pixel);
@@ -99,8 +92,8 @@ pub fn run(params: &RunParams) -> Result<Option<u32>> {
 
                     if delta.abs() > threshold {
                         if params.detect_anti_aliased_pixels
-                            && (antialiased(&left_image, x, y, width, height, &right_image)
-                                || antialiased(&right_image, x, y, width, height, &left_image))
+                            && (antialiased(&left_image, *x, *y, width, height, &right_image)
+                                || antialiased(&right_image, *x, *y, width, height, &left_image))
                         {
                             DiffResult::AntiAliased
                         } else {
@@ -115,28 +108,45 @@ pub fn run(params: &RunParams) -> Result<Option<u32>> {
             }
         };
 
-        match result {
-            DiffResult::Identical | DiffResult::BelowThreshold => {
-                if let Some(alpha) = params.blend_factor_of_unchanged_pixels {
-                    let yiq_y = YIQ::rgb2y(&left_pixel.to_rgb());
-                    let rgba_a = left_pixel.channels()[3] as f32;
-                    let color =
-                        dify::blend_semi_transparent_white(yiq_y, alpha * rgba_a / 255.0) as u8;
+        (x, y, result)
+    });
 
-                    output_image.put_pixel(x, y, Rgba([color, color, color, u8::MAX]));
-                }
-            }
-            DiffResult::Different | DiffResult::OutOfBounds => {
-                diffs += 1;
-                output_image.put_pixel(x, y, RED_PIXEL);
-            }
-            DiffResult::AntiAliased => {
-                output_image.put_pixel(x, y, YELLOW_PIXEL);
-            }
-        }
-    }
+    let diffs = results
+        .clone()
+        .fold(0, |acc, (_x, _y, result)| match result {
+            DiffResult::Identical | DiffResult::BelowThreshold | DiffResult::AntiAliased => acc,
+            DiffResult::Different | DiffResult::OutOfBounds => acc + 1,
+        });
 
     if diffs > 0 {
+        let mut output_image = match params.output_image_base {
+            Some(cli::OutputImageBase::LeftImage) => left_image.clone(),
+            Some(cli::OutputImageBase::RightImage) => right_image.clone(),
+            None => RgbaImage::new(width, height),
+        };
+
+        for (x, y, result) in results {
+            match result {
+                DiffResult::Identical | DiffResult::BelowThreshold => {
+                    if let Some(alpha) = params.blend_factor_of_unchanged_pixels {
+                        let left_pixel = left_image.get_pixel(*x, *y);
+                        let yiq_y = YIQ::rgb2y(&left_pixel.to_rgb());
+                        let rgba_a = left_pixel.channels()[3] as f32;
+                        let color =
+                            dify::blend_semi_transparent_white(yiq_y, alpha * rgba_a / 255.0) as u8;
+
+                        output_image.put_pixel(*x, *y, Rgba([color, color, color, u8::MAX]));
+                    }
+                }
+                DiffResult::Different | DiffResult::OutOfBounds => {
+                    output_image.put_pixel(*x, *y, RED_PIXEL);
+                }
+                DiffResult::AntiAliased => {
+                    output_image.put_pixel(*x, *y, YELLOW_PIXEL);
+                }
+            }
+        }
+
         output_image
             .save_with_format(params.output, ImageFormat::Png)
             .with_context(|| {
