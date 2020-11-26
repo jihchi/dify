f21570a0e4e0b39a110509d8eb20d8eb76a6ef7f
jihchi <achi@987.tw>
2020-11-26 22:10:26 +0800

    Initial shared run parameters once

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -11,70 +11,36 @@ fn get_image(path: &str) -> RgbaImage {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
+    let default_run_params = diff::RunParams {
+        left: "",
+        right: "",
+        output: "",
+        threshold: 0.1,
+        output_image_base: None,
+        do_not_check_dimensions: true,
+        detect_anti_aliased_pixels: false,
+        blend_factor_of_unchanged_pixels: None,
+    };
+
     c.bench_function("1000 × 667 pixels", |b| {
         let left_image = get_image("./benches/fixtures/tiger.jpg");
         let right_image = get_image("./benches/fixtures/tiger-2.jpg");
 
-        b.iter(|| {
-            diff::get_results(
-                &left_image,
-                &right_image,
-                &diff::RunParams {
-                    left: "",
-                    right: "",
-                    output: "",
-                    threshold: 0.1,
-                    output_image_base: None,
-                    do_not_check_dimensions: true,
-                    detect_anti_aliased_pixels: false,
-                    blend_factor_of_unchanged_pixels: None,
-                },
-            )
-        })
+        b.iter(|| diff::get_results(&left_image, &right_image, &default_run_params))
     });
 
     c.bench_function("8400 × 4725 pixels", |b| {
         let left_image = get_image("./benches/fixtures/water-4k.png");
         let right_image = get_image("./benches/fixtures/water-4k-2.png");
 
-        b.iter(|| {
-            diff::get_results(
-                &left_image,
-                &right_image,
-                &diff::RunParams {
-                    left: "",
-                    right: "",
-                    output: "",
-                    threshold: 0.1,
-                    output_image_base: None,
-                    do_not_check_dimensions: true,
-                    detect_anti_aliased_pixels: false,
-                    blend_factor_of_unchanged_pixels: None,
-                },
-            )
-        })
+        b.iter(|| diff::get_results(&left_image, &right_image, &default_run_params))
     });
 
     c.bench_function("3446 × 10728 pixels", |b| {
         let left_image = get_image("./benches/fixtures/www.cypress.io.png");
         let right_image = get_image("./benches/fixtures/www.cypress.io-2.png");
 
-        b.iter(|| {
-            diff::get_results(
-                &left_image,
-                &right_image,
-                &diff::RunParams {
-                    left: "",
-                    right: "",
-                    output: "",
-                    threshold: 0.1,
-                    output_image_base: None,
-                    do_not_check_dimensions: true,
-                    detect_anti_aliased_pixels: false,
-                    blend_factor_of_unchanged_pixels: None,
-                },
-            )
-        })
+        b.iter(|| diff::get_results(&left_image, &right_image, &default_run_params))
     });
 }
 
