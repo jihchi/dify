bfea68a109b71f887b0ead7d219aeff3c9392617
jihchi <achi@987.tw>
2020-11-26 20:39:56 +0800

    Fix a lint error

--- src/diff.rs
+++ src/diff.rs
@@ -85,7 +85,7 @@ pub fn run(params: &RunParams) -> Result<Option<u32>> {
     if diffs > 0 {
         let mut output_image = match params.output_image_base {
             Some(cli::OutputImageBase::LeftImage) => left_image.clone(),
-            Some(cli::OutputImageBase::RightImage) => right_image.clone(),
+            Some(cli::OutputImageBase::RightImage) => right_image,
             None => {
                 let (width, height) = left_dimensions;
                 ImageBuffer::new(width, height)
